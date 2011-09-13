2edfba5ec113445d4c6b898e54f2d81038658b43
Chung Hwan Kim <chunghwn@cs.utah.edu>
2011-09-13 22:22:06 +0000

    Oops, forgot to replace tabs with four spaces.

--- vmps/vmps.c
+++ vmps/vmps.c
@@ -244,7 +244,7 @@ int init_xa(xa_instance_t *xa, const char *domain)
 {
     memset(xa, 0, sizeof(*xa));
     //xa->pae = 1;
-	xa->os_type = XA_OS_LINUX; /* currently support linux only */
+    xa->os_type = XA_OS_LINUX; /* currently support linux only */
     if (strlen(conf_sysmap) > 0)
         xa->sysmap = conf_sysmap; /* use the sysmap file specified in config */
     if (xa_init_vm_name_strict((char *)domain, xa) == XA_FAILURE)
