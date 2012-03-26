b27891728cf4fa2488d1648373c77fb326f4b76f
Anton Burtsev <aburtsev@flux.utah.edu>
2012-03-26 15:26:14 -0700

    [NFS-PERF] add virtually all variables we want to access during the analysis, not all the probes get called on this trace -- I'll record an NFS session later

--- examples/nfs-perf-analysis/probes.c
+++ examples/nfs-perf-analysis/probes.c
@@ -39,6 +39,20 @@
 struct bsymbol *bsymbol_netif_poll_lvar_skb = NULL;
 struct bsymbol *bsymbol_netif_receive_skb_lvar_skb = NULL;
 struct bsymbol *bsymbol_ip_rcv_lvar_skb = NULL;
+struct bsymbol *bsymbol_tcp_v4_rcv_lvar_skb = NULL;
+struct bsymbol *bsymbol_tcp_data_queue_lvar_skb = NULL;
+struct bsymbol *bsymbol_do_readv_writev_lvar_iov = NULL;
+struct bsymbol *bsymbol_do_readv_writev_lvar_uvector = NULL;
+struct bsymbol *bsymbol_generic_file_writev_lvar_iov = NULL;
+struct bsymbol *bsymbol_generic_file_buffered_write_lvar_iov = NULL;
+struct bsymbol *bsymbol_generic_file_buffered_write_lvar_page = NULL;
+struct bsymbol *bsymbol___block_write_full_page_lvar_page = NULL;
+struct bsymbol *bsymbol___block_write_full_page_lvar_bh = NULL;
+struct bsymbol *bsymbol_submit_bh_lvar_bh = NULL;
+struct bsymbol *bsymbol_submit_bh_lvar_bio = NULL;
+struct bsymbol *bsymbol_blkif_queue_request_lvar_bio = NULL;
+struct bsymbol *bsymbol_blkif_queue_request_lvar_id = NULL;
+struct bsymbol *bsymbol_blkif_int_lvar_id = NULL;
 
 typedef enum nfs_perf_stage_id {
         STAGE_ID_NETIF_POLL              = 1, 
@@ -90,7 +104,6 @@ int probe_netif_poll_lb_skb_dequeue(struct probe *probe, void *handler_data, str
         ERR("Cannot access value of skb\n");
         return -1;
     }
-
     DBG("skb = 0x%lx\n", *(unsigned long*)lval_skb->buf);
 
 #if 0
@@ -147,7 +160,6 @@ int probe_netif_receive_skb(struct probe *probe, void *handler_data, struct prob
         ERR("Cannot access value of skb\n");
         return -1;
     }
-
     DBG("skb = 0x%lx\n", *(unsigned long*)lval_skb->buf);
 
     /* Rrequest id is address of the skb passed to the netif_skb_function */
@@ -156,6 +168,7 @@ int probe_netif_receive_skb(struct probe *probe, void *handler_data, struct prob
 }
 
 int probe_ip_rcv_init(struct probe *probe) {
+
     DBG("ip_rcv called\n");
 
     bsymbol_ip_rcv_lvar_skb = target_lookup_sym(probe->target, "ip_rcv.skb", ".", NULL, SYMBOL_TYPE_NONE); 
@@ -178,41 +191,104 @@ int probe_ip_rcv(struct probe *probe, void *handler_data, struct probe *trigger)
         ERR("Cannot access value of skb\n");
         return -1;
     }
-
     DBG("skb = 0x%lx\n", *(unsigned long*)lval_skb->buf);
     return 0;
 }
 
 int probe_tcp_v4_rcv_init(struct probe *probe) {
     DBG("tcp_v4_rcv_init called\n");
+
+    bsymbol_tcp_v4_rcv_lvar_skb = target_lookup_sym(probe->target, "tcp_v4_rcv.skb", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol_tcp_v4_rcv_lvar_skb) {
+        ERR("Failed to create a bsymbol for tcp_v4_rcv.skb\n");
+        return -1;
+    }
+
     return 0;
 };
 
 int probe_tcp_v4_rcv(struct probe *probe, void *handler_data, struct probe *trigger)
 {
+    struct value   *lval_skb;
+
     DBG("tcp_v4_rcv called\n");
+
+    lval_skb = bsymbol_load(bsymbol_tcp_v4_rcv_lvar_skb, LOAD_FLAG_NONE);
+    if (!lval_skb) {
+        ERR("Cannot access value of skb\n");
+        return -1;
+    }
+    DBG("skb = 0x%lx\n", *(unsigned long*)lval_skb->buf);
     return 0;
 }
 
 int probe_tcp_data_queue_init(struct probe *probe) {
     DBG("tcp_data_queue_init called\n");
+
+    bsymbol_tcp_data_queue_lvar_skb = target_lookup_sym(probe->target, "tcp_data_queue.skb", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol_tcp_data_queue_lvar_skb) {
+        ERR("Failed to create a bsymbol for tcp_data_queue.skb\n");
+        return -1;
+    }
+
     return 0;
 };
 
 int probe_tcp_data_queue(struct probe *probe, void *handler_data, struct probe *trigger)
 {
+    struct value   *lval_skb;
+
     DBG("tcp_data_queue called\n");
+
+    lval_skb = bsymbol_load(bsymbol_tcp_data_queue_lvar_skb, LOAD_FLAG_NONE);
+    if (!lval_skb) {
+        ERR("Cannot access value of skb\n");
+        return -1;
+    }
+    DBG("skb = 0x%lx\n", *(unsigned long*)lval_skb->buf);
+
     return 0;
 }
 
 int probe_skb_copy_datagram_iovec_init(struct probe *probe) {
     DBG("skb_copy_datagram_iovec_init called\n");
+
+    bsymbol_skb_copy_datagram_iovec_lvar_skb = target_lookup_sym(probe->target, "skb_copy_datagram_iovec.skb", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol_skb_copy_datagram_iovec_lvar_skb) {
+        ERR("Failed to create a bsymbol for skb_copy_datagram_iovec.skb\n");
+        return -1;
+    }
+
+    bsymbol_skb_copy_datagram_iovec_lvar_to = target_lookup_sym(probe->target, "skb_copy_datagram_iovec.to", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol_skb_copy_datagram_iovec_lvar_to) {
+        ERR("Failed to create a bsymbol for skb_copy_datagram_iovec.to\n");
+        return -1;
+    }
+
     return 0;
 };
 
 int probe_skb_copy_datagram_iovec(struct probe *probe, void *handler_data, struct probe *trigger)
 {
+    struct value   *lval_skb;
+    struct value   *lval_to;
+
     DBG("skb_copy_datagram_iovec called\n");
+
+    lval_skb = bsymbol_load(bsymbol_skb_copy_datagram_iovec_lvar_skb, LOAD_FLAG_NONE);
+    if (!lval_skb) {
+        ERR("Cannot access value of skb\n");
+        return -1;
+    }
+    DBG("skb = 0x%lx\n", *(unsigned long*)lval_skb->buf);
+
+    lval_to = bsymbol_load(bsymbol_skb_copy_datagram_iovec_lvar_to, LOAD_FLAG_NONE);
+    if (!lval_to) {
+        ERR("Cannot access value of to\n");
+        return -1;
+    }
+    DBG("iovec to = 0x%lx\n", *(unsigned long*)lval_to->buf);
+
     return 0;
 }
 
@@ -240,89 +316,297 @@ int probe_nfsd3_proc_write(struct probe *probe, void *handler_data, struct probe
 
 int probe_do_readv_writev_ttd_copy_from_user_init(struct probe *probe) {
     DBG("do_readv_writev_ttd_copy_from_user_init called\n");
+
+    bsymbol_do_readv_writev_lvar_uvector = target_lookup_sym(probe->target, "do_readv_writev.uvector", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol_do_readv_writev_lvar_uvector) {
+        ERR("Failed to create a bsymbol for do_readv_writev.uvector\n");
+        return -1;
+    }
+
+    bsymbol_do_readv_writev_lvar_iov = target_lookup_sym(probe->target, "do_readv_writev.iov", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol_do_readv_writev_lvar_iov) {
+        ERR("Failed to create a bsymbol for do_readv_writev.iov\n");
+        return -1;
+    }
+
     return 0;
 };
 
 int probe_do_readv_writev_ttd_copy_from_user(struct probe *probe, void *handler_data, struct probe *trigger)
 {
-    DBG("do_readv_writev_ttd_copy_from_user called\n");
+    struct value   *lval_uvector;
+    struct value   *lval_iov;
+
+    DBG("do_readv_writev.ttd_copy_from_user called\n");
+
+    lval_uvector = bsymbol_load(bsymbol_do_readv_writev_lvar_uvector, LOAD_FLAG_NONE);
+    if (!lval_uvector) {
+        ERR("Cannot access value of uvector\n");
+        return -1;
+    }
+    DBG("uvector = 0x%lx\n", *(unsigned long*)lval_uvector->buf);
+
+    lval_iov = bsymbol_load(bsymbol_do_readv_writev_lvar_iov, LOAD_FLAG_NONE);
+    if (!lval_iov) {
+        ERR("Cannot access value of iov\n");
+        return -1;
+    }
+    DBG("iov = 0x%lx\n", *(unsigned long*)lval_iov->buf);
+
     return 0;
 }
 
 int probe_generic_file_writev_init(struct probe *probe) {
     DBG("generic_file_writev_init called\n");
+
+    bsymbol_generic_file_writev_lvar_iov = target_lookup_sym(probe->target, "generic_file_writev.iov", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol_generic_file_writev_lvar_iov) {
+        ERR("Failed to create a bsymbol for generic_file_writev.iov\n");
+        return -1;
+    }
+
     return 0;
 };
 
 int probe_generic_file_writev(struct probe *probe, void *handler_data, struct probe *trigger)
 {
+    struct value   *lval_iov;
+
     DBG("generic_file_writev called\n");
+
+    lval_iov = bsymbol_load(bsymbol_generic_file_writev_lvar_iov, LOAD_FLAG_NONE);
+    if (!lval_iov) {
+        ERR("Cannot access value of iov\n");
+        return -1;
+    }
+    DBG("iov = 0x%lx\n", *(unsigned long*)lval_iov->buf);
+
     return 0;
 }
 
 int probe_generic_file_buffered_write_init(struct probe *probe) {
     DBG("generic_file_buffered_write_init called\n");
+
+    bsymbol_generic_file_buffered_write_lvar_iov 
+        = target_lookup_sym(probe->target, "generic_file_buffered_write.iov", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol_generic_file_buffered_write_lvar_iov) {
+        ERR("Failed to create a bsymbol for generic_file_buffered_write.iov\n");
+        return -1;
+    }
+
+    bsymbol_generic_file_buffered_write_lvar_page 
+        = target_lookup_sym(probe->target, "generic_file_buffered_write.page", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol_generic_file_buffered_write_lvar_page) {
+        ERR("Failed to create a bsymbol for generic_file_buffered_write.page\n");
+        return -1;
+    }
     return 0;
 };
 
 int probe_generic_file_buffered_write(struct probe *probe, void *handler_data, struct probe *trigger)
 {
+    struct value   *lval_iov;
+    struct value   *lval_page;
+
     DBG("generic_file_buffered_write called\n");
+    lval_iov = bsymbol_load(bsymbol_generic_file_buffered_write_lvar_iov, LOAD_FLAG_NONE);
+    if (!lval_iov) {
+        ERR("Cannot access value of iov\n");
+        return -1;
+    }
+    DBG("iov = 0x%lx\n", *(unsigned long*)lval_iov->buf);
+
+    lval_page = bsymbol_load(bsymbol_generic_file_buffered_write_lvar_page, LOAD_FLAG_NONE);
+    if (!lval_page) {
+        ERR("Cannot access value of page\n");
+        return -1;
+    }
+    DBG("page = 0x%lx\n", *(unsigned long*)lval_page->buf);
+
     return 0;
 }
 
 int probe_ext3_journalled_writepage_init(struct probe *probe) {
     DBG("ext3_journalled_writepage_init called\n");
+
+    bsymbol_ext3_journalled_writepage_lvar_page 
+        = target_lookup_sym(probe->target, "ext3_journalled_writepage.page", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol_ext3_journalled_writepage_lvar_page) {
+        ERR("Failed to create a bsymbol for ext3_journalled_writepage.page\n");
+        return -1;
+    }
+
     return 0;
 };
 
 int probe_ext3_journalled_writepage(struct probe *probe, void *handler_data, struct probe *trigger)
 {
+    struct value   *lval_page;
+
     DBG("ext3_journalled_writepage called\n");
+    lval_page = bsymbol_load(bsymbol_ext3_journalled_writepage_lvar_page, LOAD_FLAG_NONE);
+    if (!lval_page) {
+        ERR("Cannot access value of page\n");
+        return -1;
+    }
+    DBG("page = 0x%lx\n", *(unsigned long*)lval_page->buf);
     return 0;
 }
 
 int probe___block_write_full_page_init(struct probe *probe) {
     DBG("__block_write_full_page_init called\n");
+
+    bsymbol___block_write_full_page_lvar_page
+        = target_lookup_sym(probe->target, "__block_write_full_page.page", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol___block_write_full_page_lvar_page) {
+        ERR("Failed to create a bsymbol for __block_write_full_page.page\n");
+        return -1;
+    }
+
+    bsymbol___block_write_full_page_lvar_bh
+        = target_lookup_sym(probe->target, "__block_write_full_page.bh", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol___block_write_full_page_lvar_bh) {
+        ERR("Failed to create a bsymbol for __block_write_full_page.bh\n");
+        return -1;
+    }
+
     return 0;
 };
 
 int probe___block_write_full_page(struct probe *probe, void *handler_data, struct probe *trigger)
 {
+    struct value   *lval_page;
+    struct value   *lval_bh;
+
     DBG("__block_write_full_page called\n");
+
+    lval_page = bsymbol_load(bsymbol___block_write_full_page_lvar_page, LOAD_FLAG_NONE);
+    if (!lval_page) {
+        ERR("Cannot access value of page\n");
+        return -1;
+    }
+    DBG("page = 0x%lx\n", *(unsigned long*)lval_page->buf);
+
+    lval_bh = bsymbol_load(bsymbol___block_write_full_page_lvar_bh, LOAD_FLAG_NONE);
+    if (!lval_bh) {
+        ERR("Cannot access value of page\n");
+        return -1;
+    }
+    DBG("page = 0x%lx\n", *(unsigned long*)lval_bh->buf);
+
     return 0;
 }
 
 int probe_submit_bh_init(struct probe *probe) {
     DBG("submit_bh_init called\n");
+
+    bsymbol_submit_bh_lvar_bh
+        = target_lookup_sym(probe->target, "submit_bh.bh", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol_submit_bh_lvar_bh) {
+        ERR("Failed to create a bsymbol for submit_bh.bh\n");
+        return -1;
+    }
+
+    bsymbol_submit_bh_lvar_bio
+        = target_lookup_sym(probe->target, "submit_bh.bio", ".", NULL, SYMBOL_TYPE_NONE);
+    if (!bsymbol_submit_bh_lvar_bio) {
+        ERR("Failed to create a bsymbol for submit_bh.bio\n");
+        return -1;
+    }
+
     return 0;
 };
 
 int probe_submit_bh(struct probe *probe, void *handler_data, struct probe *trigger)
 {
+    struct value   *lval_bh;
+    struct value   *lval_bio;
+
     DBG("submit_bh called\n");
+    
+    lval_bh  = bsymbol_load(bsymbol_submit_bh_lvar_bh, LOAD_FLAG_NONE);
+    if (!lval_bh) {
+        ERR("Cannot access value of bh\n");
+        return -1;
+    }
+    DBG("bh = 0x%lx\n", *(unsigned long*)lval_bh->buf);
+
+    lval_bio  = bsymbol_load(bsymbol_submit_bh_lvar_bio, LOAD_FLAG_NONE);
+    if (!lval_bio) {
+        ERR("Cannot access value of bio\n");
+        return -1;
+    }
+    DBG("bio = 0x%lx\n", *(unsigned long*)lval_bio->buf);
     return 0;
 }
 
 int probe_blkif_queue_request_init(struct probe *probe) {
     DBG("blkif_queue_request_init called\n");
+
+    bsymbol_blkif_queue_request_lvar_bio
+        = target_lookup_sym(probe->target, "blkif_queue_request.bio", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol_blkif_queue_request_lvar_bio) {
+        ERR("Failed to create a bsymbol for blkif_queue_request.bio\n");
+        return -1;
+    }
+
+    bsymbol_blkif_queue_request_lvar_id
+        = target_lookup_sym(probe->target, "blkif_queue_request.id", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol_blkif_queue_request_lvar_id) {
+        ERR("Failed to create a bsymbol for blkif_queue_request.id\n");
+        return -1;
+    }
+
     return 0;
 };
 
 int probe_blkif_queue_request(struct probe *probe, void *handler_data, struct probe *trigger)
 {
+    struct value   *lval_bio;
+    struct value   *lval_id;
+
     DBG("blkif_queue_request called\n");
+
+    lval_bio  = bsymbol_load(bsymbol_blkif_queue_request_lvar_bio, LOAD_FLAG_NONE);
+    if (!lval_bio) {
+        ERR("Cannot access value of bh\n");
+        return -1;
+    }
+    DBG("bio = 0x%lx\n", *(unsigned long*)lval_bio->buf);
+
+    lval_id  = bsymbol_load(bsymbol_blkif_queue_request_lvar_id, LOAD_FLAG_NONE);
+    if (!lval_id) {
+        ERR("Cannot access value of id\n");
+        return -1;
+    }
+    DBG("id = 0x%lx\n", *(unsigned long*)lval_id->buf);
     return 0;
 }
 
 int probe_blkif_int_init(struct probe *probe) {
     DBG("blkif_int_init called\n");
+
+    bsymbol_blkif_int_lvar_id
+        = target_lookup_sym(probe->target, "blkif_int.id", ".", NULL, SYMBOL_TYPE_NONE); 
+    if (!bsymbol_blkif_int_lvar_id) {
+        ERR("Failed to create a bsymbol for blkif_int.id\n");
+        return -1;
+    }
     return 0;
 };
 
 int probe_blkif_int(struct probe *probe, void *handler_data, struct probe *trigger)
 {
+    struct value   *lval_id;
+
     DBG("blkif_int called\n");
+
+    lval_id  = bsymbol_load(bsymbol_blkif_int_lvar_id, LOAD_FLAG_NONE);
+    if (!lval_id) {
+        ERR("Cannot access value of id\n");
+        return -1;
+    }
+    DBG("id = 0x%lx\n", *(unsigned long*)lval_id->buf);
     return 0;
 }
 
