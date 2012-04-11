59a10d95325988efed0082b05275797ed7d0f6fe
Chung Hwan Kim <chunghwn@cs.utah.edu>
2012-04-11 00:45:02 -0600

    [CTRL-FLOW] Try instrumenting all kernel functions in a static manner
    Take a file listing function names to instrument as a command-line
    argument.  Try instrumenting all functions in the list.  Skip
    instrumenting functions of which 'target' cannot find call or return
    sites.

--- examples/ctrl-flow-analysis/ctrl-flow-static.c
+++ examples/ctrl-flow-analysis/ctrl-flow-static.c
@@ -26,10 +26,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <getopt.h>
 #include <log.h>
 
 #include <ctxprobes.h>
+#include "debug.h"
 
 extern char *optarg;
 extern int optind, opterr, optopt;
@@ -37,13 +39,79 @@ extern int optind, opterr, optopt;
 static char *domain_name = NULL; 
 static int debug_level = -1; 
 static char *sysmap_file = NULL;
+static char *funclist_file = NULL;
+
+char context_ch(ctxprobes_context_t context)
+{
+    char c;
+    switch (context) {
+        case CTXPROBES_CONTEXT_NORMAL:
+            c = 'N';
+            break;
+        case CTXPROBES_CONTEXT_TRAP:
+            c = 'T';
+            break;
+        case CTXPROBES_CONTEXT_INTERRUPT:
+            c = 'I';
+            break;
+        default:
+            c = 'X';
+            ERR("Invalid context identifier %d!\n", context);
+            break;
+    }
+    return c;
+}
+
+void probe_func_call(char *symbol, 
+                     unsigned long retaddr,
+                     ctxprobes_task_t *task,
+                     ctxprobes_context_t context)
+{
+    printf("[%c] %d (%s): %s proloque invoked\n", 
+           context_ch(context), task->pid, task->comm, symbol);
+    
+    printf("- Return address: 0x%08lx\n", retaddr);
+
+    printf("- Parent task chain: \n");
+    while (task->parent)
+    {
+        printf("  %d (%s)\n", task->parent->pid, task->parent->comm);
+        task = task->parent;
+    }
+}
+
+void probe_func_return(char *symbol, 
+                       ctxprobes_var_t *args, 
+                       int argcount, 
+                       ctxprobes_var_t *retval,
+                       unsigned long retaddr,
+                       ctxprobes_task_t *task,
+                       ctxprobes_context_t context)
+{
+    if (!retval)
+        printf("[%c] %d (%s): %s returned, but failed to load retval\n", 
+               context_ch(context), task->pid, task->comm, symbol);
+    else
+        printf("[%c] %d (%s): %s returned %d (0x%x)\n", 
+               context_ch(context), task->pid, task->comm, symbol,
+               *(int *)retval->buf, *(int *)retval->buf);
+
+    printf("- Return address: 0x%08lx\n", retaddr);
+
+    printf("- Parent task chain: \n");
+    while (task->parent)
+    {
+        printf("  %d (%s)\n", task->parent->pid, task->parent->comm);
+        task = task->parent;
+    }
+}
 
 void parse_opt(int argc, char *argv[])
 {
     char ch;
     log_flags_t debug_flags;
     
-    while ((ch = getopt(argc, argv, "dl:")) != -1)
+    while ((ch = getopt(argc, argv, "dl:m:f:")) != -1)
     {
         switch(ch)
         {
@@ -54,7 +122,8 @@ void parse_opt(int argc, char *argv[])
             case 'l':
                 if (vmi_log_get_flag_mask(optarg, &debug_flags))
                 {
-                    fprintf(stderr, "ERROR: bad debug flag in '%s'!\n", optarg);
+                    fprintf(stderr, "ERROR: bad debug flag in '%s'!\n", 
+                            optarg);
                     exit(-1);
                 }
                 vmi_set_log_flags(debug_flags);
@@ -64,6 +133,10 @@ void parse_opt(int argc, char *argv[])
                 sysmap_file = optarg;
                 break;
 
+            case 'f':
+                funclist_file = optarg;
+                break;
+                
             default:
                 fprintf(stderr, "ERROR: unknown option %c!\n", ch);
                 exit(-1);
@@ -81,9 +154,18 @@ void parse_opt(int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
-    int ret;
-
+    static FILE *fp;
+    char funcname[256];
+    int count, ret;
+    
     parse_opt(argc, argv);
+
+    fp = fopen(funclist_file, "r");
+    if (!fp)
+    {
+        ERR("Could not open function list file\n");
+        return -2;
+    }
 
     ret = ctxprobes_init(domain_name, sysmap_file, debug_level);
     if (ret)
@@ -92,24 +174,37 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    //ret = ctxprobes_func_call("sys_open", sys_open_call);
-    //if (ret)
-    //{
-    //    fprintf(stderr, "failed to register probe on sys_open call\n");
-    //    exit(1);
-    //}
+    count = 0;
+    while (fgets(funcname, 255, fp))
+    {
+        funcname[strlen(funcname)-1] = '\0';
 
-    //ret = ctxprobes_func_return("sys_open", sys_open_return);
-    //if (ret)
-    //{
-    //    fprintf(stderr, "failed to register probe on sys_open return\n");
-    //    exit(1);
-    //}
+        ret = ctxprobes_reg_func_prologue(funcname, probe_func_call);
+        if (ret)
+        {
+            WARN("Failed to register probe on %s call. Skipping...\n", 
+                 funcname);
+            continue;
+        }
 
-    printf("Starting instrumentation ...\n");
+        ret = ctxprobes_reg_func_return(funcname, probe_func_return);
+        if (ret)
+        {
+            WARN("Failed to register probe on %s return. Skipping...\n", 
+                 funcname);
+            ctxprobes_unreg_func_return(funcname, probe_func_return);
+            continue;
+        }
+
+        count++;
+    } 
+
+    printf("Total %d functions registered. Starting instrumentation...\n",
+           count);
     ctxprobes_wait();
 
     ctxprobes_cleanup();
+    fclose(fp);
     return 0;
 }
 
