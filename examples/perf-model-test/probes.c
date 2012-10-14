66df722558c0c7d9270e95408b2106049565f08f
Anton Burtsev <aburtsev@flux.utah.edu>
2012-10-14 16:21:06 -0600

    [PERF-MODEL] Reasonable version of performance test
    Time to do a nop measurement:228 (4624721608204:4624721608432)
    Time to do a nop10 measurement:52 (4624721608456:4624721608508)
    Time to do a nop100 measurement:136 (4624721608532:4624721608668)
    Time to do a nop 1 000 measurement:636 (4624721608692:4624721609328)
    Time to do a nop 10 000 measurement:10132 (4624721609352:4624721619484)
    Time to do a nop 100 000 measurement:99224 (4624721619508:4624721718732)
    Time to do a nop 1 000 000 measurement:863176 (4624721718756:4624722581932)
    
    DBG:probe_ttd_test_perf_model_nop1_end Time to do a  ## name ##  measurement:1860 (4399253947192:4399253949052)
    DBG:probe_ttd_test_perf_model_nop10_end Time to do a  ## name ##  measurement:1760 (4399253951496:4399253953256)
    DBG:probe_ttd_test_perf_model_nop100_end Time to do a  ## name ##  measurement:1788 (4399253955636:4399253957424)
    DBG:probe_ttd_test_perf_model_nop1000_end Time to do a  ## name ##  measurement:2832 (4399253959772:4399253962604)
    DBG:probe_ttd_test_perf_model_nop10000_end Time to do a  ## name ##  measurement:36560 (4399253964696:4399254001256)
    DBG:probe_ttd_test_perf_model_nop100000_end Time to do a  ## name ##  measurement:155436 (4399254006260:4399254161696)
    DBG:probe_ttd_test_perf_model_nop1000000_end Time to do a  ## name ##  measurement:1371064 (4399254166348:4399255537412)

--- examples/perf-model-test/probes.c
+++ examples/perf-model-test/probes.c
@@ -39,38 +39,53 @@
 #include "probes.h"
 #include "debug.h"
 
-unsigned long long null_start = 0;
-unsigned long long null_end = 0;
+/*
+unsigned long long null_start, null_end;
+unsigned long long nop10_start, nop10_end;
+unsigned long long nop100_start, nop100_end;
+unsigned long long nop1000_start, nop1000_end;
+unsigned long long nop10000_start, nop10000_end;
+unsigned long long nop100000_start, nop100000_end;
+unsigned long long nop1000000_start, nop1000000_end;
+*/
 
-int probe_ttd_test_perf_model_null_start_init(struct probe *probe) {
-    DBG("probe_ttd_test_perf_model_null_start_init called\n");
-    return 0;
-};
+#define PROBE_FUNC_START(name)  probe_ttd_test_perf_model_ ## name ## _start
+#define PROBE_FUNC_END(name)  probe_ttd_test_perf_model_ ## name ## _end
+#define PROBE_FUNC_INIT(name)  probe_ttd_test_perf_model_ ## name ## _init
 
-int probe_ttd_test_perf_model_null_start(struct probe *probe, void *handler_data, struct probe *trigger)
-{
-    DBG("probe_ttd_test_perf_model_null_start called\n");
-
-    null_start = perf_get_rdtsc(probe->target); 
-    return 0;
+#define DECLARE_PROBE_FUNCS(name) unsigned long long name ## _start; \
+unsigned long long name ## _end; \
+int probe_ttd_test_perf_model_ ## name ## _init \
+(struct probe *probe)\
+{ \
+    DBG("called\n"); \
+    return 0; \
+} \
+  \
+int probe_ttd_test_perf_model_ ## name ## _start \
+(struct probe *probe, void *handler_data, struct probe *trigger)\
+{ \
+    DBG("called\n"); \
+    name ## _start = perf_get_rdtsc(probe->target); \
+    return 0; \
+} \
+  \
+int probe_ttd_test_perf_model_ ## name ## _end \
+(struct probe *probe, void *handler_data, struct probe *trigger) \
+{ \
+    name ## _end = perf_get_rdtsc(probe->target); \
+    DBG("Time to do a  ## name ##  measurement:%llu (%llu:%llu)\n", \
+        name ## _end - name ## _start, name ## _start, name ## _end); \
+    return 0; \
 }
 
-int probe_ttd_test_perf_model_null_end_init(struct probe *probe) {
-    
-    DBG("probe_ttd_test_perf_model_null_end_init called\n");
-    return 0;
-};
-
-int probe_ttd_test_perf_model_null_end(struct probe *probe, void *handler_data, struct probe *trigger)
-{
-    DBG("probe_ttd_test_perf_model_null_end called\n");
-    null_end = perf_get_rdtsc(probe->target);
-    DBG("Time to do a null measurement (nop instruction):%llu (start:%llu, end:%llu)\n",
-        null_end - null_start, null_start, null_end);
-
-
-    return 0;
-}
+DECLARE_PROBE_FUNCS(nop1);
+DECLARE_PROBE_FUNCS(nop10);
+DECLARE_PROBE_FUNCS(nop100);
+DECLARE_PROBE_FUNCS(nop1000);
+DECLARE_PROBE_FUNCS(nop10000);
+DECLARE_PROBE_FUNCS(nop100000);
+DECLARE_PROBE_FUNCS(nop1000000);
 
 typedef struct probe_registration {
     char                *symbol;
@@ -79,9 +94,23 @@ typedef struct probe_registration {
 } probe_registration_t;
 
 const probe_registration_t probe_list[] = {
-    {"ttd_test_perf_model.perf_null_start",  probe_ttd_test_perf_model_null_start, {.init = probe_ttd_test_perf_model_null_start_init}},
-    {"ttd_test_perf_model.perf_null_end",   probe_ttd_test_perf_model_null_end, {.init = probe_ttd_test_perf_model_null_end_init}},
+    {"ttd_test_perf_model_perf_null_start", PROBE_FUNC_START(nop1), {.init = PROBE_FUNC_INIT(nop1)}},
+    {"ttd_test_perf_model_perf_null_end", PROBE_FUNC_END(nop1), {.init = PROBE_FUNC_INIT(nop1)}},
+    {"ttd_test_perf_model_perf_nop10_start", PROBE_FUNC_START(nop10), {.init = PROBE_FUNC_INIT(nop10)}},
+    {"ttd_test_perf_model_perf_nop10_end", PROBE_FUNC_END(nop10), {.init = PROBE_FUNC_INIT(nop10)}},
+    {"ttd_test_perf_model_perf_nop100_start", PROBE_FUNC_START(nop100), {.init = PROBE_FUNC_INIT(nop100)}},
+    {"ttd_test_perf_model_perf_nop100_end", PROBE_FUNC_END(nop100), {.init = PROBE_FUNC_INIT(nop100)}},
+    {"ttd_test_perf_model_perf_nop1000_start",PROBE_FUNC_START(nop1000), {.init = PROBE_FUNC_INIT(nop1000)}},
+    {"ttd_test_perf_model_perf_nop1000_end", PROBE_FUNC_END(nop1000), {.init = PROBE_FUNC_INIT(nop1000)}},
+    {"ttd_test_perf_model_perf_nop10000_start",PROBE_FUNC_START(nop10000), {.init = PROBE_FUNC_INIT(nop10000)}},
+    {"ttd_test_perf_model_perf_nop10000_end", PROBE_FUNC_END(nop10000), {.init = PROBE_FUNC_INIT(nop10000)}},
+    {"ttd_test_perf_model_perf_nop100000_start", PROBE_FUNC_START(nop100000), {.init = PROBE_FUNC_INIT(nop100000)}},
+    {"ttd_test_perf_model_perf_nop100000_end", PROBE_FUNC_END(nop100000), {.init = PROBE_FUNC_INIT(nop100000)}},
+    {"ttd_test_perf_model_perf_nop1000000_start", PROBE_FUNC_START(nop1000000), {.init = PROBE_FUNC_INIT(nop1000000)}},
+    {"ttd_test_perf_model_perf_nop1000000_end", PROBE_FUNC_END(nop1000000), {.init = PROBE_FUNC_INIT(nop1000000)}},
+
 };
+
 
 int register_probes(struct target *t, GHashTable *probes) {
 
