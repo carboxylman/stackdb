17a9361174fff5dbca62801a87b8283dfabb6709
David M. Johnson <johnsond@flux.utah.edu>
2013-01-10 08:58:33 -0700

    Bring copyrights up to speed.

--- include/alist.h
+++ include/alist.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2012 The University of Utah
+ * Copyright (c) 2011, 2012, 2013 The University of Utah
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
