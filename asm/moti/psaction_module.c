eb3c0d89aab461e6a3156d7bcda4c57c2e3c2c76
Prashanth Nayak <pnayak@flux.utah.edu>
2013-02-27 02:28:06 -0700

    - Initial version of kernel module equivalent of psaction.c - Having trouble setting up the tasklist_lock. Need to fix this.

---
+++ asm/moti/psaction_module.c
@@ -0,0 +1,109 @@
+#define LINUX
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/kthread.h>
+#include <linux/sched.h> 
+#include <asm/signal.h>
+#include <asm/siginfo.h>
+
+/* Assuming the module to be multi-threaded, hence may need to
+ * implement some form of a thread pool later on. As of now making
+ * use of a single task_struct variable
+ */
+static struct task_struct *thread; 
+
+/* Need to acquire a lock when trying to access the task_struct structure variables.
+ * But from 2.6.18 tasklist_lock is no longer exported in linux/sched.h. So we need
+ * to figure out how to acquire this lock
+ *
+ * extern rwlock_t tasklist_lock;
+*/
+
+
+/* As of now passing the Process Id as a module parameter.
+ * Later on a command interface needs to be implemented which
+ * can accept commands a respective parameters from the user.
+ */
+int pid;
+
+module_param(pid, int , S_IRUGO|S_IWUSR);
+
+#define SIGNAL_GROUP_EXIT       0x00000008
+#define TIF_SIGPENDING          2
+
+int check_func(int * pid)
+{
+	struct task_struct *task;
+	int found_flag = 0;
+
+
+	/* Have to set up a write lock as we are changing attribute values
+	 * of task_struct  structure.
+	 * write_lock_irq(&tasklist_lock);
+	*/
+
+	/* Iterate over all the tasks and check for a matching PID*/
+	for_each_process(task) {
+
+		if(task->pid == *pid) {
+			/* We have found the task_struct for the process*/
+			printk("Found process %s with PID = %d\n",task->comm,task->pid);
+
+			sigaddset(&task->signal->shared_pending.signal,SIGKILL);
+
+			task->signal->flags = SIGNAL_GROUP_EXIT;
+
+			task->signal->group_exit_code = 9;
+
+			task->signal->group_stop_count = 0;
+
+			/* Finally, set SIGPENDING in the task_struct's thread_info struct. */
+
+			task->thread_info->flags = TIF_SIGPENDING ;
+			printk("Killed process\n");
+			found_flag = 1;
+
+		}	
+	}
+	/*remove the lock.
+	 *write_unlock_irq(&tasklist_lock);
+	*/
+	if(!found_flag){
+			printk("Process with PID = %d not found",*pid);
+	}
+	while((!kthread_should_stop())){
+		yield();
+	}
+	return 0;
+}
+
+
+static int __init variable_check_init(void) {
+	printk("Creating a kthread in the init function\n");
+	thread = kthread_run(&check_func, &pid, "__ps_kill");
+	if(IS_ERR(thread))
+	{
+		printk("Kthread creation failed\n");
+		return -ENOMEM;
+	}
+	return 0;
+}
+
+static void __exit variable_check_exit(void)
+{
+	int result;
+	printk("In the exit function \n");
+	result = kthread_stop(thread);
+	if(result == -EINTR)
+        {
+                printk("Kthread_stop failed\n");
+        } else
+        {
+                printk(" check_func returned %d\n",result);
+        }
+}
+
+module_init(variable_check_init);
+module_exit(variable_check_exit);
