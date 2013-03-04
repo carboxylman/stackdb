ee7c3863df488f021f3d9e2bc5ee5fb3df621ee9
Prashanth Nayak <pnayak@cs.utah.edu>
2013-03-03 20:55:28 -0700

    - Added the Copyright Label. - Corrected the indentation

--- asm/moti/psaction_module.c
+++ asm/moti/psaction_module.c
@@ -1,5 +1,22 @@
-#define LINUX
+/*
+ * Copyright (c) 2011, 2012 The University of Utah
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
+#define LINUX
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -12,15 +29,14 @@
  * implement some form of a thread pool later on. As of now making
  * use of a single task_struct variable
  */
-static struct task_struct *thread; 
+static struct task_struct *thread;
 
 /* Need to acquire a lock when trying to access the task_struct structure variables.
  * But from 2.6.18 tasklist_lock is no longer exported in linux/sched.h. So we need
  * to figure out how to acquire this lock
  *
  * extern rwlock_t tasklist_lock;
-*/
-
+ */
 
 /* As of now passing the Process Id as a module parameter.
  * Later on a command interface needs to be implemented which
@@ -30,80 +46,75 @@ int pid;
 
 module_param(pid, int , S_IRUGO|S_IWUSR);
 
-#define SIGNAL_GROUP_EXIT       0x00000008
-#define TIF_SIGPENDING          2
+#define TIF_SIGPENDING 2
 
-int check_func(int * pid)
-{
-	struct task_struct *task;
-	int found_flag = 0;
+int check_func(int * pid) {
+    struct task_struct *task;
+    int found_flag = 0;
 
+    /* Have to set up a write lock as we are changing attribute values
+     * of task_struct  structure.
+     * write_lock_irq(&tasklist_lock);
+     */
 
-	/* Have to set up a write lock as we are changing attribute values
-	 * of task_struct  structure.
-	 * write_lock_irq(&tasklist_lock);
-	*/
+    /* Iterate over all the tasks and check for a matching PID*/
+    for_each_process(task) {
+        if (task->pid == *pid) {
+            int group_exit_code = 9;
+            int group_stop_count = 0;
+            unsigned int signal_group_exit = 0x00000008;
+            /* We have found the task_struct for the process*/
+            printk("Found process %s with PID = %d\n", task->comm, task->pid);
 
-	/* Iterate over all the tasks and check for a matching PID*/
-	for_each_process(task) {
+            sigaddset(&task->signal->shared_pending.signal, SIGKILL);
 
-		if(task->pid == *pid) {
-			/* We have found the task_struct for the process*/
-			printk("Found process %s with PID = %d\n",task->comm,task->pid);
+            task->signal->flags = signal_group_exit;
 
-			sigaddset(&task->signal->shared_pending.signal,SIGKILL);
+            task->signal->group_exit_code = group_exit_code;
 
-			task->signal->flags = SIGNAL_GROUP_EXIT;
+            task->signal->group_stop_count = group_stop_count;
 
-			task->signal->group_exit_code = 9;
+            /* Finally, set SIGPENDING in the task_struct's thread_info struct. */
 
-			task->signal->group_stop_count = 0;
-
-			/* Finally, set SIGPENDING in the task_struct's thread_info struct. */
-
-			task->thread_info->flags = TIF_SIGPENDING ;
-			printk("Killed process\n");
-			found_flag = 1;
-
-		}	
-	}
-	/*remove the lock.
-	 *write_unlock_irq(&tasklist_lock);
-	*/
-	if(!found_flag){
-			printk("Process with PID = %d not found",*pid);
-	}
-	while((!kthread_should_stop())){
-		yield();
-	}
-	return 0;
+            task->thread_info->flags =
+                    task->thread_info->flags | TIF_SIGPENDING;
+            printk("Killed process\n");
+            found_flag = 1;
+        }
+    }
+    /*remove the lock.
+     *write_unlock_irq(&tasklist_lock);
+     */
+    if (!found_flag) {
+        printk("Process with PID = %d not found", *pid);
+    }
+    while ((!kthread_should_stop())) {
+        yield();
+    }
+    return 0;
 }
-
 
 static int __init variable_check_init(void) {
-	printk("Creating a kthread in the init function\n");
-	thread = kthread_run(&check_func, &pid, "__ps_kill");
-	if(IS_ERR(thread))
-	{
-		printk("Kthread creation failed\n");
-		return -ENOMEM;
-	}
-	return 0;
+    printk("Creating a kthread in the init function\n");
+    thread = kthread_run(&check_func, &pid, "__ps_kill");
+    if(IS_ERR(thread)) {
+        printk("Kthread creation failed\n");
+        return -ENOMEM;
+    }
+    return 0;
 }
 
-static void __exit variable_check_exit(void)
-{
-	int result;
-	printk("In the exit function \n");
-	result = kthread_stop(thread);
-	if(result == -EINTR)
-        {
-                printk("Kthread_stop failed\n");
-        } else
-        {
-                printk(" check_func returned %d\n",result);
-        }
+static void __exit variable_check_exit(void) {
+    int result;
+
+    printk("In the exit function \n");
+    result = kthread_stop(thread);
+    if(result == -EINTR) {
+        printk("Kthread_stop failed\n");
+    } else {
+        printk(" Check_func returned %d\n",result);
+    }
 }
 
-module_init(variable_check_init);
-module_exit(variable_check_exit);
+module_init( variable_check_init);
+module_exit( variable_check_exit);
--- asm/moti/thread_module.c
+++ asm/moti/thread_module.c
@@ -1,5 +1,22 @@
-#define LINUX
+/*
+ * Copyright (c) 2011, 2012 The University of Utah
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
+#define LINUX
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -8,64 +25,50 @@
 
 static int counter;
 static int check_value;
-static struct task_struct *thread; 
-
+static struct task_struct *thread;
 
 module_param(check_value, int , S_IRUGO|S_IWUSR);
 
+int check_func(int * check_value) {
+    int i = 0;
 
-int check_func(int * check_value)
-{
-	int i = 0;
-	printk("kthread executing . . .\n");
-	while((!kthread_should_stop()))
-	{
-		if(counter == *check_value)
-		{
-			printk("The counter has changed to the required value \n");
-		}	
-		else {
-			printk("Calling yield \n");
-			yield();
-/*			if(result == -1)
-			{
-				printk("yield failed\n");
-			}
-*/
-		}
-	}
-	return 0; 
+    printk("kthread executing . . .\n");
+    while ((!kthread_should_stop())) {
+        if (counter == *check_value) {
+            printk("The counter has changed to the required value \n");
+        } else {
+            printk("Calling yield \n");
+            yield();
+        }
+    }
+    return 0;
 
 }
 
+static int __init variable_check_init(void) {
+    counter = 10;
 
-static int __init variable_check_init(void)
-{
-	printk("Initializing the counter variable to 10 \n");
-	counter = 10;
-	printk("Creating a kthread in the init function\n");
-	thread = kthread_run(&check_func, &check_value, "check_func_thread");
-	if(IS_ERR(thread))
-	{
-		printk("Kthread_run error\n");
-		return -ENOMEM;
-	}
-	printk("Thread created and running successfully\n");
-	return 0;
+    printk("Initializing the counter variable to 10 \n");
+    printk("Creating a kthread in the init function\n");
+    thread = kthread_run(&check_func, &check_value, "check_func_thread");
+    if(IS_ERR(thread)) {
+        printk("Kthread_run error\n");
+        return -ENOMEM;
+    }
+    printk("Thread created and running successfully\n");
+    return 0;
 }
-static void __exit variable_check_exit(void)
-{
-	int result;
-	printk("In the exit function, killing the kthread\n");
-	 result = kthread_stop(thread);
-	if(result == -EINTR)
-	{
-		printk("Kthread_stop failed\n");
-	} else
-	{
-		printk(" check_func returned %d\n",result);
-	}
+static void __exit variable_check_exit(void) {
+    int result;
+
+    printk("In the exit function, killing the kthread\n");
+    result = kthread_stop(thread);
+    if(result == -EINTR) {
+        printk("Kthread_stop failed\n");
+    } else {
+        printk(" check_func returned %d\n",result);
+    }
 }
 
-module_init(variable_check_init);
-module_exit(variable_check_exit);
+module_init( variable_check_init);
+module_exit( variable_check_exit);
