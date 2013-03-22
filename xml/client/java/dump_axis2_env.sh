e70b154c3117dfc194a4564600dd948142e289cf
Mike Hibler <hibler@cs.utah.edu>
2013-03-22 05:48:31 -0600

    Use '.' not 'source' for sourcing script.

--- xml/client/java/dump_axis2_env.sh
+++ xml/client/java/dump_axis2_env.sh
@@ -4,7 +4,7 @@ if test -z "${AXIS2_HOME}"; then
     exit 1
 fi
 
-source ${AXIS2_HOME}/bin/setenv.sh
+. ${AXIS2_HOME}/bin/setenv.sh
 
 echo "AXIS2_HOME=${AXIS2_HOME}"
 echo "AXIS2_CLASSPATH=${AXIS2_CLASSPATH}"
