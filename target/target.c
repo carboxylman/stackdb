0367d11786ed59ca796cde5d0c60d5a1627c77a5
David M. Johnson <johnsond@flux.utah.edu>
2013-07-08 14:37:14 -0600

    Actually insert memmod into tracking hashtables.

--- target/target.c
+++ target/target.c
@@ -3314,6 +3314,11 @@ struct target_memmod *target_memmod_create(struct target *target,tid_t tid,
 	return NULL;
     }
 
+    if (paddr)
+	g_hash_table_insert(target->phys_mmods,(gpointer)paddr,mmod);
+    else
+	g_hash_table_insert(target->phys_mmods,(gpointer)addr,mmod);
+
     array_list_append(mmod->threads,tthread);
 
     vdebug(5,LA_TARGET,LF_TARGET,
