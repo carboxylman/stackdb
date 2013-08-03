a1a6cd761c90093ef657cad8d4036fc357685cb8
David M. Johnson <johnsond@flux.utah.edu>
2013-08-03 09:31:37 -0600

    Add overlay support to cfi_check.
    If you specify an overlay, the symbols you pass on the command line
    (the root symbols in which initial CFI probes are placed) apply only
    to the overlay target, of course.
    
    (This stuff needs to be factored out, somehow, because there's lots of
    shared code to instantiate an overlay target, but it's not clear quite
    how to do it because overlays are pretty custom to the application
    using them.  Well, ok, in 10 seconds of thinking it's more clear, but
    this works for now.)

--- tools/cfi/cfi_check.c
+++ tools/cfi/cfi_check.c
@@ -42,6 +42,7 @@
 #include "cfi.h"
 
 struct target *target = NULL;
+struct target *otarget = NULL;
 struct probe *cfi_probe = NULL;
 
 static int result_counter = 0;
@@ -52,6 +53,11 @@ target_status_t cleanup() {
     if (cfi_probe) {
 	probe_free(cfi_probe,1);
 	cfi_probe = NULL;
+    }
+    if (otarget) {
+	target_close(otarget);
+	target_free(otarget);
+	otarget = NULL;
     }
     if (target) {
 	retval = target_close(target);
@@ -156,11 +162,14 @@ struct cc_argp_state {
     cfi_flags_t flags;
     int argc;
     char **argv;
+    char *overlay_name_or_id;
+    struct target_spec *overlay_spec;
 };
 
 struct cc_argp_state opts;
 
 struct argp_option cc_argp_opts[] = {
+    { "overlay",'O',"<name_or_id>:<spec_opts>",0,"Lookup name or id as an overlay target once the main target is instantiated, and try to open it.  All spec_opts (normal target/dwdebug opts) then apply to the overlay target.",0 },
     { "mode",'M',"dynamic|static",0,"Set the CFI mode (only dynamic now).",-3 },
     { "no-autofollow",'N',NULL,0,
         "Do not add functions to the CFI checked set.",-3 },
@@ -174,6 +183,13 @@ struct argp_option cc_argp_opts[] = {
 error_t cc_argp_parse_opt(int key,char *arg,struct argp_state *state) {
     struct cc_argp_state *opts = \
 	(struct cc_argp_state *)target_argp_driver_state(state);
+    struct array_list *argv_list;
+    char *argptr;
+    char *nargptr;
+    char *vargptr;
+    int inesc;
+    int inquote;
+    int quotechar;
 
     switch (key) {
     case ARGP_KEY_ARG:
@@ -200,6 +216,107 @@ error_t cc_argp_parse_opt(int key,char *arg,struct argp_state *state) {
     case ARGP_KEY_ERROR:
     case ARGP_KEY_FINI:
 	return 0;
+    case 'O':
+	/*
+	 * We need to split the <name_or_id>:<spec> part; then split
+	 * <spec> into an argv.  Simple rules: \ escapes the next char;
+	 * space not in ' or " causes us to end the current argv[i] and
+	 * start the next one.
+	 */
+	argptr = index(arg,':');
+	if (!argptr) {
+	    verror("bad overlay spec!\n");
+	    return EINVAL;
+	}
+	argv_list = array_list_create(32);
+	array_list_append(argv_list,"dumptarget_overlay");
+
+	opts->overlay_name_or_id = arg;
+	*argptr = '\0';
+	++argptr;
+
+	while (*argptr == ' ')
+	    ++argptr;
+
+	inesc = 0;
+	inquote = 0;
+	quotechar = 0;
+	nargptr = argptr;
+	vargptr = argptr;
+	while (*argptr != '\0') {
+	    if (*argptr == '\\') {
+		if (inesc) {
+		    inesc = 0;
+		    *nargptr = '\\';
+		    ++nargptr;
+		}
+		else {
+		    /* Don't copy the escape char. */
+		    inesc = 1;
+		    ++argptr;
+		    continue;
+		}
+	    }
+	    else if (inesc) {
+		inesc = 0;
+		/* Just copy it. */
+		*nargptr = *argptr;
+		++nargptr;
+	    }
+	    else if (inquote && *argptr == quotechar) {
+		/* Ended the quoted sequence; don't copy quotes. */
+		inquote = 0;
+		quotechar = 0;
+		++argptr;
+		continue;
+	    }
+	    else if (*argptr == '\'' || *argptr == '"') {
+		inquote = 1;
+		quotechar = *argptr;
+		++argptr;
+		continue;
+	    }
+	    else if (!inquote && *argptr == ' ') {
+		*nargptr = *argptr = '\0';
+		if (vargptr) {
+		    array_list_append(argv_list,vargptr);
+		    //printf("vargptr (%p) = '%s'\n",vargptr,vargptr);
+		    vargptr = NULL;
+		}
+		vargptr = NULL;
+		nargptr = ++argptr;
+		continue;
+	    }
+	    else {
+		if (!vargptr)
+		    vargptr = nargptr;
+
+		*nargptr = *argptr;
+		++nargptr;
+	    }
+
+	    /* Default increment. */
+	    ++argptr;
+	}
+	if (vargptr) {
+	    *nargptr = '\0';
+	    array_list_append(argv_list,vargptr);
+	    //printf("vargptr (%p) = '%s'\n",vargptr,vargptr);
+	}
+	array_list_append(argv_list,NULL);
+
+	opts->overlay_spec = target_argp_driver_parse(NULL,NULL,
+						      array_list_len(argv_list) - 1,
+						      (char **)argv_list->list,
+						      TARGET_TYPE_XEN_PROCESS,0);
+	if (!opts->overlay_spec) {
+	    verror("could not parse overlay spec!\n");
+	    array_list_free(argv_list);
+	    return EINVAL;
+	}
+
+	array_list_free(argv_list);
+	break;
 
     default:
 	return ARGP_ERR_UNKNOWN;
@@ -219,6 +336,10 @@ int main(int argc,char **argv) {
     struct array_list *root_function_list;
     int i;
     struct bsymbol *function;
+    struct target *rtarget;
+    int oid;
+    tid_t otid;
+    char *tmp = NULL;
 
     target_init();
     atexit(target_fini);
@@ -234,7 +355,7 @@ int main(int argc,char **argv) {
 	exit(-1);
     }
 
-    target = target_instantiate(tspec,NULL);
+    rtarget = target = target_instantiate(tspec,NULL);
     if (!target) {
 	verror("could not instantiate target!\n");
 	exit(-1);
@@ -246,10 +367,47 @@ int main(int argc,char **argv) {
 	exit(-4);
     }
 
+    /*
+     * Load an overlay target, if there is one.
+     */
+    if (opts.overlay_name_or_id && opts.overlay_spec) {
+	errno = 0;
+	oid = (int)strtol(opts.overlay_name_or_id,&tmp,0);
+	if (errno || tmp == opts.overlay_name_or_id)
+	    otid = 
+		target_lookup_overlay_thread_by_name(target,
+						     opts.overlay_name_or_id);
+	else
+	    otid = target_lookup_overlay_thread_by_id(target,oid);
+	if (otid < 0) {
+	    verror("could not find overlay thread '%s', exiting!\n",
+		   opts.overlay_name_or_id);
+	    cleanup();
+	    exit(-111);
+	}
+	otarget = target_instantiate_overlay(target,otid,opts.overlay_spec);
+	if (!otarget) {
+	    verror("could not instantiate overlay target '%s'!\n",
+		   opts.overlay_name_or_id);
+	    cleanup();
+	    exit(-112);
+	}
+
+	if (target_open(otarget)) {
+	    fprintf(stderr,"could not open overlay target!\n");
+	    cleanup();
+	    exit(-114);
+	}
+
+	target_tostring(target,targetstr,sizeof(targetstr));
+
+	rtarget = otarget;
+    }
+
     if (opts.argc > 0) {
 	root_function_list = array_list_create(opts.argc);
 	for (i = 0; i < opts.argc; ++i) {
-	    function = target_lookup_sym(target,opts.argv[i],NULL,NULL,
+	    function = target_lookup_sym(rtarget,opts.argv[i],NULL,NULL,
 					 SYMBOL_TYPE_FLAG_NONE);
 	    if (!function) {
 		verror("could not lookup symbol %s; aborting!\n",opts.argv[i]);
@@ -261,14 +419,14 @@ int main(int argc,char **argv) {
     }
     else {
 	root_function_list = array_list_create(1);
-	function = target_lookup_sym(target,"ap_mpm_run",NULL,NULL,
+	function = target_lookup_sym(rtarget,"ap_mpm_run",NULL,NULL,
 				     SYMBOL_TYPE_FLAG_NONE);
 	if (!function) {
-	    function = target_lookup_sym(target,"main",NULL,NULL,
+	    function = target_lookup_sym(rtarget,"main",NULL,NULL,
 					 SYMBOL_TYPE_FLAG_NONE);
 	    if (!function) {
 		vwarn("could not lookup symbol main; trying __libc_start_main!\n");
-		function = target_lookup_sym(target,"__libc_start_main",
+		function = target_lookup_sym(rtarget,"__libc_start_main",
 					     NULL,NULL,SYMBOL_TYPE_FLAG_NONE);
 		if (!function) {
 		    verror("could not lookup symbol __libc_start_main;"
@@ -299,7 +457,7 @@ int main(int argc,char **argv) {
     signal(SIGUSR2,sigh);
 
     /* Install probes... */
-    cfi_probe = probe_cfi(target,TID_GLOBAL,opts.mode,opts.flags,
+    cfi_probe = probe_cfi(rtarget,TID_GLOBAL,opts.mode,opts.flags,
 			  root_function_list,
 			  cfi_handler,cfi_handler,NULL);
     if (!cfi_probe) {
