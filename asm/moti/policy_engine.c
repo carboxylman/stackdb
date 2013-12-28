b1a27e867ba4114509c9f4131c5ac7e0cb249e37
Prashanth Nayak <pnayak@flux.utah.edu>
2013-12-27 19:32:09 -0700

    Tested the policy_engine implementation. 	First VMI based probing tool that collects information regarding executing processes and encodes them as facts in a file.

--- asm/moti/policy_engine.c
+++ asm/moti/policy_engine.c
@@ -24,110 +24,290 @@
  *	   recovery actions
  */
 
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <errno.h>
 #include <string.h>
-#include "clipssrc/clips.h"
+#include <sys/time.h>
+#include <sys/user.h>
+#include <sys/ptrace.h>
+#include <inttypes.h>
 
+#include <signal.h>
 
-char generate_timestamp(char *date) {
+#include <argp.h>
 
-    time_t t= time(0);
+#include "log.h"
+#include "dwdebug.h"
+#include "target_api.h"
+#include "target.h"
+#include "target_xen_vm.h"
+
+#include "probe_api.h"
+#include "probe.h"
+#include "alist.h"
+#include "list.h"
+#include "policy_engine.h"
+//#include "clipssrc/clips.h"
+
+struct target *target = NULL;
+char base_fact_file[100];
+
+target_status_t cleanup() {
+
+    target_status_t retval;
+    retval = target_close(target);
+    target_free(target);
+    return retval;
+}
+
+int generate_timestamp(char *date) {
+
+    time_t t;
     struct tm *tm;
-    tm = gmtime(&t);
-    int result = 0
-    result = strftime(date,sizeof(date), "%Y%m%d%H%M%S", tm);
-    
+    int result = 0;
+
+    time(&t);
+    tm = localtime(&t);
+    result = strftime(date,100, "%Y_%m_%d_%H_%M_%S.fac", tm);
+
     return result;
 }
+
+int generate_snapshot() {
+
+    int result = 0;
+    target_status_t status;
+
+    /* Pause the target */
+    if ((status = target_status(target)) != TSTATUS_PAUSED) {
+	fprintf(stdout,"INFO: Pausing the target\n");
+	if (target_pause(target)) {
+		fprintf(stderr,"Failed to pause the target \n");
+		result = 1;
+		goto resume;
+	 }
+    }
+
+			    
+    // start making calls to each of the VMI function 
+    result = process_info();
+    if(result) {
+	fprintf(stdout,"ERROR: process_info function failed\n");
+	result = 1;
+	goto resume;
+    }   
+                                    
+resume:
+
+    if ((status = target_status(target)) == TSTATUS_PAUSED) {
+	fprintf(stdout,"INFO: Resuming the target\n");
+	if (target_resume(target)) {
+	    fprintf(stdout, "ERROR: Failed to resume target.\n ");
+	    result = 1;
+	}
+    }
+    return result;
+}
+
+
+
+struct psa_argp_state {
+    int argc;
+    char **argv;
+    /* Grab this from the child parser. */
+    struct target_spec *tspec;
+};
+
+struct psa_argp_state opts;
+
+struct argp_option psa_argp_opts[] = {
+        { 0,0,0,0,0,0 },
+};
+
+
+
+error_t psa_argp_parse_opt(int key,char *arg,struct argp_state *state) {
+    struct psa_argp_state *opts = \
+	(struct psa_argp_state *)target_argp_driver_state(state);
+	    
+    switch (key) {
+    case ARGP_KEY_ARG:
+	return ARGP_ERR_UNKNOWN;
+    case ARGP_KEY_ARGS:
+	/* Eat all the remaining args. */
+	if (state->quoted > 0)
+	    opts->argc = state->quoted - state->next;
+	else
+	    opts->argc = state->argc - state->next;
+	if (opts->argc > 0) {
+	    opts->argv = calloc(opts->argc,sizeof(char *));
+	    memcpy(opts->argv,&state->argv[state->next],opts->argc*sizeof(char *));
+	    state->next += opts->argc;
+	}
+	return 0;
+    case ARGP_KEY_INIT:
+	target_driver_argp_init_children(state);
+	return 0;
+    case ARGP_KEY_END:
+    case ARGP_KEY_NO_ARGS:
+    case ARGP_KEY_SUCCESS:
+	opts->tspec = target_argp_target_spec(state);
+	return 0;
+    case ARGP_KEY_ERROR:
+    case ARGP_KEY_FINI:
+	return 0;
+
+    default:
+	return ARGP_ERR_UNKNOWN;
+    }
+
+    return 0;
+}
+
+struct argp psa_argp = {
+        psa_argp_opts,psa_argp_parse_opt,NULL,NULL,NULL,NULL,NULL,
+};
+
+
+
+
 
 
 int main( int argc, char** argv) {
 
     int wait_time = 0;
     char *app_file_path = NULL;
-    char base_fact_file[10];
-    char recovery_fact_file[100;
+    char recovery_fact_file[100];
     int result = 0;
+    char targetstr[80];
+    struct target_spec *tspec = NULL;
+    target_status_t tstat;
 
 
-    // Check if file name and the wait_time have been passed as arguments
-    if( argc != 3) {
-	fprintf(stdout,
-		"ERROR: Two arguments need\n 1. File path \n 2. Wait time\n");
+    memset(&opts,0,sizeof(opts));
+    tspec = target_argp_driver_parse(NULL,NULL,argc,argv,TARGET_TYPE_XEN,1);
+    if (!tspec) {
+	fprintf(stdout,"ERROR: Could not parse target arguments!\n");
+	exit(-1);
+    }
+	        
+    /*if (opts.argc != 2) {
+	fprintf(stderr,"ERROR: Must supply the 2 arguments.\n 1. Application knowlege file\n 2. Wait time.\n");
 	exit(0);
     }
-    app_file_path = argv[0];
-    wait_time = atoi(argv[1]);
+   */
+    app_file_path = "test"; //opts.argv[0];
+    wait_time = 300; //atoi(opts.argv[1]);
+    
+    dwdebug_init();
+    target_init();
+    atexit(target_fini);
+    atexit(dwdebug_fini);
+
+
+     target = target_instantiate(tspec,NULL);
+     if (!target) {
+	fprintf(stdout,"ERROR: Could not instantiate target!\n");
+	exit(0);
+     }
+     //target_snprintf(target,targetstr,sizeof(targetstr));
+    if (target_open(target)) {
+	fprintf(stdout,"ERROR: Could not open %s!\n",targetstr);
+	exit(0);
+    }
+
 
     // Initialize the CLIPS environment
-    InitializeEnvironment();
-    fprintf(stdout,"INFO: Loading the application level rules\n");
-    Load(app_file_path);
-    if(result != 1) {
-	fprintf(stdout,"ERROR: Failed to load the application rules file\n");
-	exit(0);
-    }
-    fprintf(stdout,"INFO: Loading the recovery rules.\n");
-    result = Load("recovery_rules.clp");
-    if(result != 1) {
-	fprintf(stdout," ERROR: Failed to load the recovery rules file\n");
-	exit(0);
-    }
+    /*InitializeEnvironment();
+      fprintf(stdout,"INFO: Loading the application level rules\n");
+      Load(app_file_path);
+      if(result != 1) {
+      fprintf(stdout,"ERROR: Failed to load the application rules file\n");
+      exit(0);
+      }
+      fprintf(stdout,"INFO: Loading the recovery rules.\n");
+      result = Load("recovery_rules.clp");
+      if(result != 1) {
+      fprintf(stdout," ERROR: Failed to load the recovery rules file\n");
+      exit(0);
+      }
+     */
 
-    // Start an infinite loop to periodically execute steps 31. to 3.5 
+    // Start an infinite loop to periodically execute steps 3.1 to 3.5 
     while(1) {
 
 	// Generate a time stamp for the base facts file name
 	result = generate_timestamp(base_fact_file);
-	if!(result) {
+	if(!result) {
 	    fprintf(stdout,"Failed to generate timestamp");
 	    exit(0);
 	}
+	fprintf(stdout," INFO: Base fact file name  = %s\n",base_fact_file);
 
 	// Make call to the base VMI  base function. This function invokes all the 
 	// VMI tools that gather state information of the virtual appliance/
-	
-	result = generate_snapshot(base_fact_file);
+
+	result = generate_snapshot();
 	if( result) {
-	    fprintf(stdout,"ERROR: Failed to generate the system snapshot.\n
-		     Trying again...\n");
+	    fprintf(stdout,"ERROR: Failed to generate the system snapshot.\n \
+		    Trying again...\n");
 	    continue;
 	}
-
-	fprintf(stdout"INFO: Loading the base facts file\n");
-	result = LoadFacts(date);
-	if(!result) {
-	    fprintf(stdout,"ERROR: Failed to load the base factsf file.\n");
-	    exit(0);
-	}
-	fprintf(stdout,"INFO: Resetting the CLIPS environemnt\n");
-	Reset();
-	fprintf(stdout,"INFO: Parsing the base facts through the application rules\n");
-	result = Run(-1L);
-	fprintf(stdout,"INFO : %d application rules were fired\n",result);
+	/*
+	   fprintf(stdout"INFO: Loading the base facts file\n");
+	   result = LoadFacts(base_fact_file);
+	   if(!result) {
+	   fprintf(stdout,"ERROR: Failed to load the base facts file.\n");
+	   exit(0);
+	   }
+	   fprintf(stdout,"INFO: Resetting the CLIPS environemnt\n");
+	   Reset();
+	   fprintf(stdout,"INFO: Parsing the base facts through the application rules\n");
+	   result = Run(-1L);
+	   fprintf(stdout,"INFO : %d application rules were fired\n",result);
 	// At this time the anomaly facts are generated.
 	// We have to run them through the recovery rules now.
-	
+
 	result = Run(-1L);
 	fprintf(stdout,"INFO : %d recovery rules were fired\n",result);
 
 	result = generate_timestamp(recovery_fact_file);
 	if(!result){
-	    fprintf(stdout,"ERROR: Failed to generate a timestamp\n");
-	    exit(0);
+	fprintf(stdout,"ERROR: Failed to generate a timestamp\n");
+	exit(0);
 	}
 	result = SaveFacts(recovery_fact_file,  VISIBLE_SAVE, NULL);
 	if(!result) {
-	    fprintf(stdout,"ERROR: Failed to save the recovery facts\n");
-	    exit(0);
+	fprintf(stdout,"ERROR: Failed to save the recovery facts\n");
+	exit(0);
 	}
 	// Now based on the recovery facts that are generated we trigger recovery actions.
 	// how do we do this ?
-	
 
+	 */
+	fprintf(stdout," Sleeping for %d seconds\n", wait_time);
 	sleep(wait_time);
+    }
+
+
+
+exit:
+    fflush(stderr);
+    fflush(stdout);
+    tstat = cleanup();
+    if(tstat == TSTATUS_DONE) {
+	printf(" Monitoring finished.\n");
+	return 0;
+    }
+    else if (tstat == TSTATUS_ERROR) {
+	printf("Monitoring failed!\n");
+	return 1;
+    }
+    else {
+	printf("Monitoring failed with %d!\n",tstat);
+	return 1;
     }
 
 }
--- asm/moti/policy_engine.h
+++ asm/moti/policy_engine.h
@@ -16,114 +16,108 @@
  * Foundation, 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h>
-
-#include <sys/user.h>
-#include <sys/ptrace.h>
-#include <inttypes.h>
-
-#include <signal.h>
-
-#include <argp.h>
-
-#include "log.h"
-#include "dwdebug.h"
-#include "target_api.h"
-#include "target.h"
-#include "target_xen_vm.h"
-
-#include "probe_api.h"
-#include "probe.h"
-#include "alist.h"
-#include "list.h"
 
 
-struct target *t = NULL;
-char * base_fact_file = NULL;
+extern struct target *target;    
+extern char base_fact_file[100]; 
 
-
-
-int ps_garther(struct target *target, struct value * value, void * data) {
+int ps_gather(struct target *target, struct value * value, void * data) {
 
     struct value *pid_v;
-    int pid;
+    unsigned int pid;
     struct value *uid_v;
-    int uid;
+    unsigned int uid;
     struct value *name_v;
-    char name[35];
+    char *name;
     struct value *euid_v;
-    int euid;
+    unsigned int euid;
     struct value *suid_v;
-    int suid;
+    unsigned int suid;
     struct value *fsuid_v;
-    int fsuid;
+    unsigned int fsuid;
     struct value *gid_v;
-    int gid;
+    unsigned int gid;
     struct value *egid_v;
-    int egid;
+    unsigned int egid;
     struct value *sgid_v;
-    int sgid;
+    unsigned int sgid;
     struct value * fsgid_v;
-    int fdgid;
+    unsigned int fsgid;
+
+    struct value * real_cred_v;
+    ADDR real_cred_addr;
+    struct symbol *cred_struct_type = NULL;
+    struct value *new_value;
     FILE * fp;
 
     pid_v = target_load_value_member(target, NULL, value, "pid", NULL, LOAD_FLAG_NONE);
     pid = v_i32(pid_v);
     name_v = target_load_value_member(target, NULL, value, "comm", NULL, LOAD_FLAG_NONE);
     name = strdup(name_v->buf);
-    //not sure if this is going to work for latest kernel version becuse of the new cred structure
-    uid_v = target_load_value_member(target, NULL, value, "uid", NULL, LOAD_FLAG_NONE);
-    uid = v_i32(uid_v);
-    euid_v = target_load_value_member(target, NULL, value, "euid", NULL, LOAD_FLAG_NONE);
-    euid = v_i32(euid_v);
-    suid_v = target_load_value_member(target, NULL, value, "suid", NULL, LOAD_FLAG_NONE);
-    suid = v_i32(suid_v);
-    fsuid_v = target_load_value_member(target, NULL, value, "fsuid", NULL, LOAD_FLAG_NONE);
-    fsuid = v_i32(fsuid_v);
-    gid_v = target_load_value_member(target, NULL, value, "gid", NULL, LOAD_FLAG_NONE);
-    gid = v_i32(gid_v);
-    egid_v = target_load_value_member(target, NULL, value, "egid", NULL, LOAD_FLAG_NONE);
-    egid = v_i32(egid_v);
-    sgid_v = target_load_value_member(target, NULL, value, "sgid", NULL, LOAD_FLAG_NONE);
-    sgid = v_i32(sgid_v);
-    fsgid_v = target_load_value_member(target, NULL, value, "fsgid" NULL, LOAD_FLAG_NONE);
-    fsgid = v_i32(fsgid_v);
+    
+    real_cred_v = target_load_value_member(target, NULL, value, "real_cred", NULL, LOAD_FLAG_NONE);
+    real_cred_addr = value_addr(real_cred_v);
+
+    cred_struct_type = bsymbol_get_symbol(target_lookup_sym(target, "struct cred",
+			NULL, "cred", SYMBOL_TYPE_FLAG_TYPE));
+    
+    new_value = target_load_type(target, cred_struct_type,real_cred_addr, LOAD_FLAG_NONE);
+    if(!new_value) {
+	fprintf(stdout,"ERROR: Failed to load type of struct cred.\n");
+	return 1;
+    }
+
+
+    uid_v = target_load_value_member(target, NULL, new_value, "uid", NULL, LOAD_FLAG_NONE);
+    uid = v_u32(uid_v);
+    euid_v = target_load_value_member(target, NULL, new_value, "euid", NULL, LOAD_FLAG_NONE);
+    euid = v_u32(euid_v);
+    suid_v = target_load_value_member(target, NULL, new_value, "suid", NULL, LOAD_FLAG_NONE);
+    suid = v_u32(suid_v);
+    fsuid_v = target_load_value_member(target, NULL, new_value, "fsuid", NULL, LOAD_FLAG_NONE);
+    fsuid = v_u32(fsuid_v);
+    gid_v = target_load_value_member(target, NULL, new_value, "gid", NULL, LOAD_FLAG_NONE);
+    gid = v_u32(gid_v);
+    egid_v = target_load_value_member(target, NULL, new_value, "egid", NULL, LOAD_FLAG_NONE);
+    egid = v_u32(egid_v);
+    sgid_v = target_load_value_member(target, NULL, new_value, "sgid", NULL, LOAD_FLAG_NONE);
+    sgid = v_u32(sgid_v);
+    fsgid_v = target_load_value_member(target, NULL, new_value, "fsgid", NULL, LOAD_FLAG_NONE);
+    fsgid = v_u32(fsgid_v);
+   
     // now populate the base fact into the file.
 
+
     fp = fopen(base_fact_file, "a+");
-    if(fp ==NULL) {
+    if(fp == NULL) {
 	fprintf(stdout," ERROR: Failed to open the base fact file\n");
 	exit(0);
     }
     // first write the template of for the fact
     // OR should this go into the application knowledge file ???
-    fprintf(fp,"(deftemplate task-struct\n
-			\t(slot comm (type STRING))\n
-			\t(slot pid (type INTEGER))\n
-			\t(slot uid (type INTEGER))\n
-			\t(slot euid (type INTEGER))\n
-			\t(slot suid (type INTEGER))\n
-			\t(slot fsuid (type INTEGER))\n
-			\t(slot gid (type INTEGER))\n
-			\t(slot egid (type INTEGER))\n
-			\t(slot sgid (type INTEGER))\n
-			\t(slot fsgid (type INTEGER))\n
-		    )");
+    fprintf(fp,"\n(deftemplate task-struct\n \
+			\t(slot comm (type STRING))\n \
+			\t(slot pid (type INTEGER))\n \
+			\t(slot uid (type INTEGER))\n \
+			\t(slot euid (type INTEGER))\n \
+			\t(slot suid (type INTEGER))\n \
+			\t(slot fsuid (type INTEGER))\n \
+			\t(slot gid (type INTEGER))\n \
+			\t(slot egid (type INTEGER))\n \
+			\t(slot sgid (type INTEGER))\n \
+			\t(slot fsgid (type INTEGER)))");
     // Now populate the base fact
-    fprintf(fp,"(task-struct\n
-		    \t(comm \"%s\")\n
-		    \t(pid %d)\n
-		    \t(uid %d)\n
-		    \t(euid %d)\n
-		    \t(suid %d)\n
-		    \t(fsuid %d)\n
-		    \t(gid %d)\n
-		    \t(egid %d)\n
-		    \t(sgid %d)\n
-		    \t(fsgid %d)\n",name,pid,gid,uid,euid,suid,fsuid,gid,egid,sgid,fsgid);
+    fprintf(fp,"\n(task-struct\n \
+		    \t(comm \"%s\")\n \
+		    \t(pid %u)\n \
+		    \t(uid %u)\n \
+		    \t(euid %u)\n \
+		    \t(suid %u)\n \
+		    \t(fsuid %u)\n \
+		    \t(gid %u)\n \
+		    \t(egid %u)\n \
+		    \t(sgid %u)\n \
+		    \t(fsgid %u)\n",name,pid,uid,euid,suid,fsuid,gid,egid,sgid,fsgid);
     
     fclose(fp);
 
@@ -133,81 +127,22 @@ int ps_garther(struct target *target, struct value * value, void * data) {
 
 int process_info() {
     
+    int ret_val; 
     struct bsymbol *init_task_bsymbol;
 
-    init_task_bsymbol = target_lookup_symbol(t,"init_task",NULL,NULL,
+    init_task_bsymbol = target_lookup_sym(target,"init_task",NULL,NULL,
 						    SYMBOL_TYPE_FLAG_VAR);
     if(!init_task_bsymbol) {
 	fprintf(stdout,"ERROR: Could not lookup the init_task symbol\n");
 	return 1;
     }
 
-    linux_list_for_each_struct(t, init_task_bsymbol, "tasks",0,ps_gather, NULL);
+    fprintf(stdout," file name = %s\n", base_fact_file);
 
-    return 0;
-
-}
-
-
-
-
-
-int generate_snapshot(char* file_name) {
+    ret_val = linux_list_for_each_struct(target, init_task_bsymbol, "tasks",0,ps_gather, NULL);
     
-    int result;
-    struct target_spec * tspec;
-
-    base_fact_file = filename;
-    tspec = target_argp_driver_parse(NULL, NULL, argc, argv,
-	                TARGET_TYPE_XEN, 1);
-
-    if (!tspec) {
-	fprintf(stdout,"ERROR: Could not parse target arguments!\n");
-	exit(-1);
-    }
-
-    dwdebug_init();
-    atexit(dwdebug_fini);
-
-    /* Initialize the target */
-    t = target_instantiate(tspec,NULL);
-    if (!t) {
-	fprintf(stdout,"ERROR: Count not instantiate target.\n");
-	exit(-1);
-    }
-    target_snprintf(t,targetstr,sizeof(targetstr));
-
-    /* Open connection to the target.*/
-    if (target_open(t)) {
-	fprintf(stdout, "ERROR: Connection to target failed.\n");
-	exit(-1);
-    }
-
-    // start making calls to each of the VMI function 
-    result = process_info();
-    if(result) {
-	fprintf(stdout,"ERROR: process_info function failed\n");
-	goto exit
-    }
-
-
-
-
-exit:
-    fflush(stderr);
-    fflush(stdout);
-    tstat = cleanup();
-    if(tstat == TSTATUS_DONE) {
-         printf("%s finished.\n",targetstr);
-	 return 0;
-    }
-    else if (tstat == TSTATUS_ERROR) {
-	printf("%s monitoring failed!\n",targetstr);
-	return 1;
-    }
-    else {
-	printf("%s monitoring failed with %d!\n",targetstr,tstat);
-	return 1;
-    }
+    return ret_val;
 
 }
+
+
